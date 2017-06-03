aacf26346a410fc4a0c9be35711f494df54d726c
Stefan Henneken <stefan.henneken@live.com>
2017-06-03 23:11:52 +0200

    Add project files.

---
+++ CarBMW/BMW.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using CarContract;
+
+namespace CarBMW
+{
+    [ExportMetadata("Name","BMW")]
+    [ExportMetadata("Color", CarColor.Black)]
+    [ExportMetadata("Price", (uint)55000)]
+    [Export(typeof(ICarContract))]
+    public class BMW : ICarContract
+    {
+        private BMW()
+        {
+            Console.WriteLine("BMW constructor.");
+        }
+        public string StartEngine(string name)
+        {
+            return String.Format("{0} starts the BMW.", name);
+        }
+    }
+}
